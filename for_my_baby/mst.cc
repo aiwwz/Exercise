558ddee51ffa46914a829700b565a0269b6361f1
AIWWZ <jxzh1023@163.com>
2019-07-27 11:53:20 -0300

    王志军: 实现最小生成树

---
+++ for_my_baby/mst.cc
@@ -0,0 +1,14 @@
+/*********************************************
+* file:   mst.cc
+* author: AIWWZ(wzj1524@qq.com)
+* date:   2019-07-27 11:52:22
+**********************************************/
+#include <iostream>
+using namespace std;
+
+
+ 
+int main() {
+ 
+    return 0;
+}
